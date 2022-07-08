7639327399522f07b8f892007ecb92bbf05d0aad
Alexander Dergunov <raphop27@gmail.com>
2022-07-08 11:03:01 +0300

    model1

--- Model.js
+++ Model.js
@@ -1,7 +1,20 @@
+const fs = require('fs');
+const { monitorEventLoopDelay } = require('perf_hooks');
+
 class Model {
-  constructor() {
-
+  constructor(questions) {
+    this.questions = questions
+    this.scores = 0
   }
+  read(path){
+  fs.readFile(path, 'UTF-8',(err, data)=>{
+if(err) throw new   Error('без грубостей')
+console.log(data);
+  }); 
+ 
+ 
+ }  
 }
-
+const model = new Model()
+model.read('./topics/nighthawk_flashcard_data.txt')
 module.exports = Model
