8ffb699afb4573c3ff8dcf36de2a3d99d5c4a2f6
Tekutos <tekuto1993@gmail.com>
2022-07-08 11:02:04 +0300

    release 0.1 view

--- View.js
+++ View.js
@@ -1,7 +1,12 @@
-class View {
-  constructor() {
+const { stdout } = require('process');
+const readline = require('readline')
+readline.createInterface(
+  input: process.stdin,
+  output: process/stdout
+)
 
-  }
+class View {
+  constructor() {}
 }
 
-module.exports = View
+module.exports = View;
