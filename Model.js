43533bd791848c53488786d9c0e33666e5084dd5
Alexander Dergunov <raphop27@gmail.com>
2022-07-08 14:05:17 +0300

    toPromises

--- Model.js
+++ Model.js
@@ -7,21 +7,32 @@ class Model {
     this.scores = 0
   }
 
-  read(path){
-    let result = []
-    let v = fs.readFileSync(path, 'UTF-8').split('\n')
-    for (let i = 0; i < v.length; i++) {
-      if (i % 2 ===0) result.push({q: v[i], a: v[i + 1]}) 
-    }
-    return result    
+  read(path){  
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, 'UTF-8', (err, data) =>{
+        if (err) return reject(err)
+        let result = []
+        let v = data.split('\n')
+        for (let i = 0; i < v.length; i++) {
+          if (i % 2 ===0) result.push({q: v[i], a: v[i + 1]}) 
+        }
+        return resolve(result)
+      })
+    })
   }
 
   getTopics(path){
-    return fs.readdirSync(path)
+    return new Promise((resolve, reject) => {
+      fs.readdir(path, (err, files) => {
+        if(err) return reject(err)
+        return resolve(files)
+      })
+    })
   }
  }
 
 
 const model = new Model()
-// console.log(model.read('./topics/nighthawk_flashcard_data.txt'))
+model.read('./topics/nighthawk_flashcard_data.txt').then(x => console.log(x))
+model.getTopics('./topics').then(x => console.log(x))
 module.exports = Model
